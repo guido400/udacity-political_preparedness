14bef75364a0faa76abba7e584d4d0e223f0c375
Guido <guidoroos@protonmail.com>
2021-06-14 08:44:12 +0200

    fix: fixed issue with api call for voter info

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -4,28 +4,33 @@ import com.example.android.politicalpreparedness.BuildConfig
 import com.example.android.politicalpreparedness.MyApplication
 import com.example.android.politicalpreparedness.R
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
         private val apiKey = BuildConfig.API_KEY
+        private val logging = HttpLoggingInterceptor()
 
         fun getClient(): OkHttpClient {
+            logging.level = HttpLoggingInterceptor.Level.BASIC
+
             return Builder()
-                    .addInterceptor { chain ->
-                        val original = chain.request()
-                        val url = original
-                                .url()
-                                .newBuilder()
-                                .addQueryParameter("key", apiKey)
-                                .build()
-                        val request = original
-                                .newBuilder()
-                                .url(url)
-                                .build()
-                        chain.proceed(request)
-                    }
+                .addInterceptor(logging)
+                .addInterceptor{ chain ->
+                    val original = chain.request()
+                    val url = original
+                        .url
+                        .newBuilder()
+                        .addQueryParameter("key", apiKey)
+                        .build()
+                    val request = original
+                        .newBuilder()
+                        .url(url)
+                        .build()
+                    chain.proceed(request)
+                }
                     .build()
         }
 
